ad96be66e887c0c5b0f5fa794088444946b8640e
Johannes Elferich <elferich@ohsu.edu>
2015-05-25 20:47:34 -0700

    Build sequence tree

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+	grunt.registerTask( 'js', [ 'jshint'] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
