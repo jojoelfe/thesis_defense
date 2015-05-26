fc68fc0791cce7254e147a18c0abd75e9bc4924e
Johannes Elferich <elferich@ohsu.edu>
2015-05-26 16:12:08 -0700

    Added HMM SVG

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'default', [ 'css', 'js' ] );
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint'] );
+	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
